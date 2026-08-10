7b5fa67c5140ed2795cade686ee306ed643cf7c5
Your Name <you@example.com>
2026-08-10 14:54:14 +0530

    g

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,14 @@
 import { tour } from "../models/Model.js"
+import crypto from "crypto"
 
 export const allTours = async (req, res) => {
     const tours = await tour.find({})
+    const cacheTag = crypto
+        .createHash("sha1")
+        .update(JSON.stringify(tours))
+        .digest("hex")
+
+    res.set("X-Tours-Cache-Tag", cacheTag)
     // console.log("all tours: ", tours)
     if (tours.length === 0) return res.status(200).json({ message: "no tours", tours: [] })
     res.status(200).json({
